d079a3dc1d7a977194a1fe2c175045de42c6c939
ngirase10 <ngirase@andrew.cmu.edu>
2024-01-23 19:06:58 -0500

    made necessary variables private

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,8 +1,8 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
+    private double height;
+    private double width;
     
     public Rectangle(double height, double width){
         this.height = height;
@@ -12,7 +12,7 @@ public class Rectangle implements Shape {
     public double getWidth() {
         return width;
     }
-    
+
     public double getHeight() {
         return height;
     }
